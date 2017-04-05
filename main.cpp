b3d7ac2e7c14bae00a365ed616bc476f03cf070b
Cody Kinniburgh <codakinn@ut.utm.edu>
2017-04-05 14:09:24 -0500

    Fourth commit: adding updated main for integer summation

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+int sum(int num)
+{
+  int total = 0;
+  for(int i = 1; i <= num; i++)
+    total = total + i;
+  return total;
+}
+
 int main()
 {
-  cout << "Hello World!" << endl;
+  int userEntry = 0;
+  cout << "Please enter a number: ";
+  cin >> userEntry;
+  cout << "The sum of integers up to that is:" << sum(userEntry) << endl;
+  
   return 0;
 }
