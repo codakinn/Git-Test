a8eae13a03d8d98bf0fb615037172a1b6d94aa6f
Cody Kinniburgh <codakinn@ut.utm.edu>
2017-04-05 14:22:34 -0500

    Sixth commit: modified main to display a product of integers

--- main.cpp
+++ main.cpp
@@ -10,12 +10,21 @@ int sum(int num)
   return total;
 }
 
+int product(int num)
+{
+  int total = 1;
+  for(int i = 1; i <= num; i++)
+    total = total * i;
+  return total;
+}
+
 int main()
 {
   int userEntry = 0;
   cout << "Please enter a number: ";
   cin >> userEntry;
-  cout << "The sum of integers up to that is:" << sum(userEntry) << endl;
+  cout << "The sum of integers up to that is: " << sum(userEntry) << endl;
+  cout << "The product of integers up to that is: " << product(userEntry) << endl;
   
   return 0;
 }
